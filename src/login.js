8f2dccd6f058531b758b14fa93cd07c4c74792ad
yoncaaa <yoncaakceylan@web.de>
2019-12-14 23:37:00 +0100

    1.

--- src/login.js
+++ src/login.js
@@ -10,3 +10,32 @@ function swfromreg(){
     reg.style.display = "none";
     log.style.display = "initial";
 }
+function selectedTab(tabIndex) {
+    //Hide all Tabs
+    document.getElementById('neuerContent').style.display= "none";
+    document.getElementById('aktuellerContent').style.display= "none";
+    //Show selected Tab
+    document.getElementById(tabIndex).style.display= "block";
+}
+function create(){
+    let newElement=document.createElement('div');
+
+    document.getElementById('neuerContent').appendChild(newElement);
+}
+function selectedTabKursu(tabIndex) {
+    //Hide all Tabs
+    document.getElementById('allekurse').style.display= "none";
+    document.getElementById('freizeit').style.display= "none";
+
+
+
+    //Show selected Tab
+    document.getElementById(tabIndex).style.display= "block";
+}
+
+function selectedTaba(id) {
+    //Hide all Tabs
+    document.getElementById('neuerContent').style.display= "none";
+    document.getElementById('aktuellerContent').style.display= "none";
+    //Show selected Tab
+    document.getElementById(id).style.display= "block";
